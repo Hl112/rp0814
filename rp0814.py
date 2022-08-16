85724305d009178f2fe265199d8f1bb97e1498fa
Forest <forest.tl112@gmail.com>
2022-08-16 22:34:36 +0700

    linInter

--- rp0814.py
+++ rp0814.py
@@ -23,7 +23,7 @@ df_lon, df_lat, df_alt = (df['lon'].values,
                           df['lat'].values,
                           df['alt'].values)
 
-points = np.stack((df_lon, df_lat, df_alt), axis=1)
+points = np.stack((df_lon, df_lat, df_alt), axis=1) 
 # and put the moisture corresponding data values in a separate array:
 values = np.array([0.205, 0.197, 0.204, 0.197, 0.212,
                    0.208, 0.204, 0.205, 0.211, 0.215])
@@ -33,10 +33,10 @@ request = np.array([[25, 20, -30], [27, 20, -32]])
 print(griddata(points, values, request))
 
 # First, define an interpolator function
-# linInter = LinearNDInterpolator(points, values)
+linInter = LinearNDInterpolator(points, values)
 
 # Then, apply the function to one or more points
-# print(linInter(np.array([[25, 20, -30]])))
+print(linInter(np.array([[25, 20, -30]])))
 # print(linInter(request))
 # OUTPUT: [0.20448536  0.20782028]
 # I think you may use it with python map or pandas.apply as well
